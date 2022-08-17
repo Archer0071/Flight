0e527f4b940e0e7044726c7050d9a6c1bb78fac4
Adil <adilking0071@gmail.com>
2022-08-17 12:32:51 +0500

    Added ViewModel

---
+++ Flight/ViewModels/StationsViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  StationsViewModel.swift
+//  Flight
+//
+//  Created by ARCHER on 17/08/2022.
+//
+
+import Foundation
+import Combine
+import UIKit
+
+class StationsViewModel: ObservableObject {
+    
+    @Published  var stations: Stations?
+    @Published  var listLoadingError: String = ""
+    @Published  var showAlert: Bool = false
+    private var cancellables: Set<AnyCancellable> = []
+    let dataService : ServiceProtocol
+    
+    init(dataService:ServiceProtocol) {
+        self.dataService = dataService
+        getLaunchesList()
+        
+    }
+    
+    func getLaunchesList() {
+        dataService.fetchStations()
+            .sink { (dataResponse) in
+                if dataResponse.error != nil {
+                    self.createAlert(with: dataResponse.error!)
+                } else {
+                    if let data = dataResponse.value {
+                        self.stations = data
+                    }
+                    
+                }
+            }.store(in: &cancellables)
+    }
+    
+    func createAlert( with error: NetworkError ) {
+        listLoadingError = error.backendError == nil ? error.initialError.localizedDescription : error.backendError!.message
+        self.showAlert = true
+        
+    }
+}
+
