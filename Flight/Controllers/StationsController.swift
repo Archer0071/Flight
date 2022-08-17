54c208e8872dcd6b24804867382f30e3c3ff29b3
Adil <adilking0071@gmail.com>
2022-08-17 12:34:22 +0500

    Added StationsController DI for ViewModel

--- Flight/Controllers/StationsController.swift
+++ Flight/Controllers/StationsController.swift
@@ -9,6 +9,16 @@ import Foundation
 import UIKit
 class StationsController:UIViewController{
     
+    var viewModel : StationsViewModel
+    init(viewModel:StationsViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "STATIONS"
